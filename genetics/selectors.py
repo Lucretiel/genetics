0801d02d4fe32f275aea1df73fa522a3454517f4
Nathan West <Lucretiel@gmail.com>
2013-08-04 18:06:55 -0400

    Minor logic updates in selectors.py
    Moved score accumulation to a separate function
    Now use islice(count, n) instead of zip(count, range(n)) in stochastic

--- genetics/selectors.py
+++ genetics/selectors.py
@@ -10,12 +10,16 @@ def tournament(tournament_size):
     return tournament_selector
 
 
+def _accumulate_scores(population):
+    scores = list(itertools.accumulate(member.score for member in population))
+    return scores, scores[-1]
+
+
 def roulette(population, num_parents):
     #Uses code taken from
     #http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
 
-    cumulative_scores = list(itertools.accumulate(member.score for member in population))
-    total = cumulative_scores[-1]
+    cumulative_scores, total = _accumulate_scores(population)
 
     for _ in range(num_parents):
         rand = random.random() * total
@@ -23,11 +27,10 @@ def roulette(population, num_parents):
 
 
 def stochastic(population, num_parents):
-    cumulative_scores = list(itertools.accumulate(member.score for member in population))
-    total = cumulative_scores[-1]
+    cumulative_scores, total = _accumulate_scores(population)
     average = total / num_parents
 
     rand = random.random() * average
 
-    for float_index, _ in zip(itertools.count(rand, average), range(num_parents)):
+    for float_index, in itertools.islice(itertools.count(rand, average), num_parents):
         yield population[bisect.bisect(cumulative_scores, float_index)]
