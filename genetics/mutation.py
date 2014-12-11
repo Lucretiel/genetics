f11829da6702d58ed51b3b482708556cab0b79cb
Lucretiel <Lucretiel@users.noreply.github.com>
2014-12-11 17:26:12 -0500

    Minor style improvement to mutation_rate

--- genetics/mutation.py
+++ genetics/mutation.py
@@ -3,5 +3,5 @@ import random
 
 def mutation_rate(rate):
     def mutation_mask(length):
-        return (True if random.random() < rate else False for _ in range(length))
+        return (random.random() < rate for _ in range(length))
     return mutation_mask
